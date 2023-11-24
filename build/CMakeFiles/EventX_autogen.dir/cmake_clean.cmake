f537efc573c0cc33e0a216d05468637712171de6
aymene <aymene@aymene.local>
2023-11-24 14:11:28 +0100

    .gitignore modifié + full fonctionnel

---
+++ build/CMakeFiles/EventX_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EventX_autogen"
+  "EventX_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/EventX_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
