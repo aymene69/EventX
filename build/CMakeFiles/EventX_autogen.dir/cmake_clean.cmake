e0dd1e4f9388edaae243cc5e99040b48fb477864
aymene <aymene@aymene.local>
2023-11-24 14:15:15 +0100

    cleaned

--- build/CMakeFiles/EventX_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/EventX_autogen"
-  "EventX_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/EventX_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
