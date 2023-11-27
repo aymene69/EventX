6e74ea88dfc0cb031505121c31ff8a25068a3083
aymene <aymene@Macbook>
2023-11-27 11:41:11 +0100

    RC Commit

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -5,10 +5,13 @@
 #include "include/GestionManager.hpp"
 #include "include/FonctionsJson.hpp"
 #include "include/FonctionsDemarrage.hpp"
-#include "include/ViewData.hpp"
 #include <QStandardPaths>
 #include <QObject>
 #include <QVBoxLayout>
+#include <QPushButton>
+#include <QStandardItemModel>
+#include <QTreeView>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     pPushButtonManageEvent = new QPushButton("Gérer l'événement", this);
@@ -89,7 +92,7 @@ void MainWindow::createLayout() {
     QObject::connect(pPushButtonManageParticipant, &QPushButton::clicked, this, &MainWindow::handleGestionParticipant);
     QObject::connect(pPushButtonManageStand, &QPushButton::clicked, this, &MainWindow::handleGestionStand);
     QObject::connect(pPushButtonManageManager, &QPushButton::clicked, this, &MainWindow::handleGestionManager);
-    QObject::connect(pPushButtonViewData, &QPushButton::clicked, this, &MainWindow::visualiser);
+    QObject::connect(pPushButtonViewData, &QPushButton::clicked, this, &MainWindow::viewData);
     QObject::connect(pPushButtonExit, &QPushButton::clicked, this, &MainWindow::close);
 }
 
@@ -121,9 +124,95 @@ void MainWindow::handleGestionManager() {
     gestionManagerDialog.exec();
 }
 
-void MainWindow::visualiser() {
-    // On affiche la fenêtre de visualisation des données
-    viewDataDialog.exec();
+void MainWindow::viewData() {
+    // Créer la fenêtre de visualisation des données
+    QDialog viewDataDialog(this);
+    viewDataDialog.setWindowTitle("EventX - Visualisation des donnees");
+    viewDataDialog.setMinimumSize(500, 300);
+
+    // Créer le modèle et la vue pour afficher les données en arbre
+    QStandardItemModel* pStandardItemModelData = new QStandardItemModel(&viewDataDialog);
+    QTreeView* pTreeViewData = new QTreeView(&viewDataDialog);
+    pTreeViewData->setHeaderHidden(true);
+    pTreeViewData->setModel(pStandardItemModelData);
+
+    // Charger les données depuis le fichier JSON
+    json jsonData = preloadData();
+
+    // Check for events before trying to display data
+    if (getNbEvents() == 0) {
+        QMessageBox::warning(this, "Avertissement", "Aucun événement à afficher.");
+        return;
+    } else {
+        // Browse events
+        for (const auto &event : jsonData["events"]) {
+            QStandardItem *pStandardItemEventItem = new QStandardItem(
+                    QString("Evenement - Date: %1, Lieu: %2, Nom: %3")
+                            .arg(QString::fromStdString(event["date"].get<std::string>()))
+                            .arg(QString::fromStdString(event["lieu"].get<std::string>()))
+                            .arg(QString::fromStdString(event["nom"].get<std::string>())));
+            pStandardItemModelData->appendRow(pStandardItemEventItem);
+            // Check for participants before browsing participants
+            if (getNbParticipantsFromEvent(event) == 0) {
+                QStandardItem *pStandardItemNoParticipants = new QStandardItem("Aucun participant");
+                pStandardItemEventItem->appendRow(pStandardItemNoParticipants);
+            } else {
+                QStandardItem *pStandardItemParticipants = new QStandardItem("Participants");
+                pStandardItemEventItem->appendRow(pStandardItemParticipants);
+                for (const auto &participant: event["participants"]) {
+                    QStandardItem *pStandardItemParticipant = new QStandardItem(
+                            QString("Email: %1, Nom: %2, Numero: %3, VIP: %4")
+                                    .arg(QString::fromStdString(participant["email"].get<std::string>()))
+                                    .arg(QString::fromStdString(participant["nom"].get<std::string>()))
+                                    .arg(QString::fromStdString(participant["numero"].get<std::string>()))
+                                    .arg(participant["vip"].get<bool>() ? "Oui" : "Non"));
+                    pStandardItemParticipants->appendRow(pStandardItemParticipant);
+                }
+            }
+            // Check if there are any pits before going through them
+            if (getNbStandsFromEvent(event) == 0) {
+                QStandardItem *pStandardItemNoStands = new QStandardItem("Aucun stand");
+                pStandardItemEventItem->appendRow(pStandardItemNoStands);
+                QStandardItem *pStandardItemNoManagers = new QStandardItem("Aucun manager");
+                pStandardItemNoStands->appendRow(pStandardItemNoManagers);
+            } else {
+                // Browse the event stands
+                QStandardItem *pStandardItemStands = new QStandardItem("Stands");
+                pStandardItemEventItem->appendRow(pStandardItemStands);
+                for (const auto &stand: event["stands"]) {
+                    QStandardItem *pStandardItemStand = new QStandardItem(QString("Nom: %1, Numero: %2, Surface: %3")
+                                                                                  .arg(QString::fromStdString(
+                                                                                          stand["nom"].get<std::string>()))
+                                                                                  .arg(stand["numero"].get<int>())
+                                                                                  .arg(QString::number(
+                                                                                          stand["surface"].get<double>())));
+                    pStandardItemStands->appendRow(pStandardItemStand);
+                    if (getNbManagersFromStand(stand) == 0) {
+                        QStandardItem *pStandardItemNoManagers = new QStandardItem("Aucun manager");
+                        pStandardItemStand->appendRow(pStandardItemNoManagers);
+                    } else {
+                        // Browse stand managers
+                        QStandardItem *pStandardItemManagers = new QStandardItem("Managers");
+                        pStandardItemStand->appendRow(pStandardItemManagers);
+                        for (const auto &manager: stand["managers"]) {
+                            QStandardItem *pStandardItemManager = new QStandardItem(
+                                    QString("ID: %1, Nom: %2, Numero: %3")
+                                            .arg(manager["id"].get<int>())
+                                            .arg(QString::fromStdString(manager["nom"].get<std::string>()))
+                                            .arg(QString::fromStdString(manager["numero"].get<std::string>())));
+                            pStandardItemManagers->appendRow(pStandardItemManager);
+                        }
+                    }
+                }
+            }
+        }
+        QVBoxLayout *pViewDataLayout = new QVBoxLayout(&viewDataDialog);
+        pViewDataLayout->addWidget(pTreeViewData);
+        viewDataDialog.setLayout(pViewDataLayout);
+
+        // Afficher la fenêtre de visualisation des données (non bloquante)
+        viewDataDialog.exec();
+    }
 }
 
 void MainWindow::handleDataModified() {
